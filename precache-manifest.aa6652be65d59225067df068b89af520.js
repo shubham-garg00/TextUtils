b4913f538647e3afdd9e352d12a0e0a960abd287
shubham-garg00 <78252047+shubham-garg00@users.noreply.github.com>
2022-10-02 17:30:12 +0530

    Updates

---
+++ precache-manifest.aa6652be65d59225067df068b89af520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2781e22dd13f5cb5ab21",
+    "url": "/TextUtils/static/css/main.5045850b.chunk.css"
+  },
+  {
+    "revision": "2781e22dd13f5cb5ab21",
+    "url": "/TextUtils/static/js/main.2781e22d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "ca71b5337f4a247b03ccb66f9b18a2d3",
+    "url": "/TextUtils/index.html"
+  }
+];
